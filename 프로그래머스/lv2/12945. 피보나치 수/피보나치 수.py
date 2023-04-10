df371c84baf7a4ff875e794b078c7ea4b036f28e
yoosoonil <97111793+yoosoonil@users.noreply.github.com>
2023-04-10 20:17:46 +0900

    [level 2] Title: 피보나치 수, Time: 513.35 ms, Memory: 439 MB -BaekjoonHub

--- 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.py
+++ 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.py
@@ -5,6 +5,16 @@ def solution(n):
     fibo_num_list = [0, 1]
     
     for i in range(n-1):
+        # n = 3
+        # n-1 = 2
+        # i = 0, 1
+        # fibo_num_list = [0, 1] 
+        # fibo_num_list[0] + fibo_num_list[1] = 0 + 1 = 1
+        # fibo_num_list = [0, 1, 1]
+        # fibo_num_list[1] + fibo_num_list[2] = 1 + 1 = 2
+        # fibo_num_list = [0, 1, 1, 2]
         fibo_num_list.append(fibo_num_list[i] + fibo_num_list[i+1])
         
+        # fibo_num_list[3] = 2
+        # 2 % 1234567 = 2
     return fibo_num_list[n] % 1234567
