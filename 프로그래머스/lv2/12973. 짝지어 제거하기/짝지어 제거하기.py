0fb8675aa530697cf6febc1f6f800d08610b0e21
yoosoonil <97111793+yoosoonil@users.noreply.github.com>
2023-05-04 10:43:24 +0900

    [level 2] Title: 짝지어 제거하기, Time: 133.59 ms, Memory: 15 MB -BaekjoonHub

--- 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.py
+++ 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.py
@@ -1,20 +1,17 @@
 def solution(s):
     stack = []
     
-    for i in s:
-        if len(stack) == 0: 
+    for i in s: # s = baabaa
+        if len(stack) == 0: # stack 리스트에 요소가 없다면
             stack.append(i)
-        elif stack[-1] == i:
-            stack.pop()
+        elif stack[-1] == i: # stack 리스트 맨뒤 요소가 s의 i랑 같다면
+            stack.pop() # stack 맨뒤 요소 삭제
         else:
-            stack.append(i)
-    if len(stack) == 0:
+            stack.append(i) # 그 외 -> stack에 요소가 하나라도 있고, 맨뒤 요소가 i랑 같지 않을 때
+    if len(stack) == 0: # 결국, 짝 지어져서 stack에는 요소가 하나도 없다면 1 return
         return 1
     else:
-        return 0
+        return 0 # stack에 요소가 하나라도 남아있다면 0 return
         
-
-    # [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-    print('Hello Python')
 
     return answer
